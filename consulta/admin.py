a92388f9b9f8dd7a2446b955412532c4ab106449
sthefanySF <sthefanysantos0815@gmail.com>
2024-01-18 12:23:18 -0300

    atualização

--- consulta/admin.py
+++ consulta/admin.py
@@ -22,10 +22,15 @@ class CampoProfissionaldasaude(admin.ModelAdmin):
 
 class CampoAgendamento(admin.ModelAdmin):
     search_fields = ['paciente']
-    list_display = ('paciente', 'data_agendamento')    
+    list_display = ('paciente', 'data_agendamento')  
+
+class CampoAtendimento(admin.ModelAdmin):
+    search_fields = ['paciente__nome']  # Substitua 'paciente__nome' pelo campo correto
+    list_display = ('paciente', 'data_atendimento', 'anamnese', 'exame_fisico')    
 
 
 admin.site.register(Paciente, CampoPaciente)
 admin.site.register(Administrativo, CampoAdministrativo)
 admin.site.register(Profissionaldasaude, CampoProfissionaldasaude)
 admin.site.register(Agendamento, CampoAgendamento)
+admin.site.register(Atendimento, CampoAtendimento)
